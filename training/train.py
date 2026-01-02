a0f4da6171c3803e1922aa56d86005e06b5e0cb0
FrankKirkham <fpmkirkham@outlook.com>
2026-01-02 19:41:17 +0000

    Added validation and model saving to train.py

--- training/train.py
+++ training/train.py
@@ -4,32 +4,15 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm # For progress bars
 from utils.preprocessing import load_data
+from utils.utils import SimpleFCNN
 
-class SimpleFCNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        # Define our layers
-        self.l1 = nn.Linear(784, 128) # 784 pixels for 28x28 MNIST image
-        self.l2 = nn.Linear(128, 10) # 10 possible numbers (0-9)   
-    
-    def forward(self, x):
-        # Flatten layer of data so it is in correct format
-        # (1, 784) instead of (1, 28, 28)
-        x = torch.flatten(x, 1)
-
-        # Hidden layer
-        x = nn.functional.relu(self.l1(x))
-        # Output layer
-        x = self.l2(x)
-        return x
-
-def train(neural_network, train_loader, epochs, learn_rate):
+def train(neural_network, train_loader, val_loader, epochs, learn_rate):
     # Set the model/nn to work on the set device (allows us to use GPU)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     neural_network = neural_network.to(device)
 
     # Set up a simple gradient descent optimiser
-    optimizer = torch.optim.SGD(neural_network.parameters(), lr=learn_rate)
+    optimizer = torch.optim.Adam(neural_network.parameters(), lr=learn_rate)
     # Set up a cross entropy loss function
     criterion = nn.CrossEntropyLoss()
 
@@ -42,19 +25,55 @@ def train(neural_network, train_loader, epochs, learn_rate):
             images, labels = images.to(device), labels.to(device)
             # Feed the data into the model
             optimizer.zero_grad()
-            outputs = neural_network.forward(images)
+            outputs = neural_network(images)
             # Apply loss function
             loss = criterion(outputs, labels)
             # Backpropagate
             loss.backward()
             optimizer.step()
 
+    print("--------------\nTraining complete!\n--------------")
+    # Validation
+    correct = 0
+    total = 0
+    with torch.no_grad(): # Don't need gradients for classifying
+        for images, labels in tqdm(val_loader, desc="Running Validation On Model"):
+            # Make sure they are running on the same device
+            images, labels = images.to(device), labels.to(device)
+            
+            # Set the model to eval mode as not to alter weights
+            neural_network.eval()
+            # Perform forward pass
+            outputs = neural_network(images)
+            # Classify result
+            results = neural_network.classify(outputs)
+
+            for i in range(len(labels)):
+                total += 1
+                if results[i] == labels[i]:
+                    correct += 1
+
+    accuracy = 100 * (correct / total)
+    print(f"--------------\nValidation Results: {accuracy:.2f}% correct ({(100 - accuracy):.2f}% loss)")
+
 def main():
-    train_loader, val_loader, test_loader, means, stds = load_data()
+    train_loader, val_loader, _, means, stds = load_data()
 
     neural_network = SimpleFCNN()
 
-    train(neural_network, train_loader, 1000, 0.001)
+    # Train the model
+    train(neural_network, train_loader, val_loader, 1, 0.001) # 1 IS USED FOR TESTING, CHANGE LATER!
+
+    # Save trained model and stats to a file to be used later:
+    model_with_stats = {
+        "model": neural_network.state_dict(),
+        "means": means,
+        "stds": stds
+    }
+
+    torch.save(model_with_stats, "model_with_stats.pth")
+    print("--------------\nModel saved")
+
 
 
 if __name__ == "__main__":
